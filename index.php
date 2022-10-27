8cc4336852a9398710cd2c6e1bea767fdcb2997f
Lucas <lucasalexandre_82@hotmail.com>
2022-10-27 17:32:10 -0300

    Dividindo mais alguns arquivos em pastas e iniciando estilização do header.

--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 <?php
 
-include_once("config/url.php");
+include_once("templates/header.php");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<h1>Testando Agenda</h1>
+<i class="fas fa-eye"></i>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda de Contatos</title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css" integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
-</head>
+<?php
 
-<body>
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
-</body>
+include_once("templates/footer.php");
 
-</html>
+?>
