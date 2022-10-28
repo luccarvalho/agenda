20e10e0e82daeb7f538a3e73b07354f3df8b9f8b
Lucas <lucasalexandre_82@hotmail.com>
2022-10-28 12:54:11 -0300

    Criando estrutura da página home com select de dados no MySql.

--- index.php
+++ index.php
@@ -4,8 +4,18 @@ include_once("templates/header.php");
 
 ?>
 
-<h1>Testando Agenda</h1>
-<i class="fas fa-eye"></i>
+<div class="container">
+    <?php if (isset($printMsg) && $printMsg != '') : ?>
+        <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
+    <h1 id="main-title">Minha Agenda</h1>
+    <?php if (count($contacts) > 0) : ?>
+        <p>Tem Contatos</p>
+    <?php else : ?>
+        <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>.</p>
+
+    <?php endif; ?>
+</div>
 
 <?php
 
